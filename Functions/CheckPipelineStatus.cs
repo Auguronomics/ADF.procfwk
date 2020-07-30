666158d5bba2be0f9eff66a6c33d199fb90c26fa
Paul Andrew <paul@mrpaulandrew.com>
2020-07-30 09:51:31 +0100

    Refactoring ready for different orchestrators

--- Functions/CheckPipelineStatus.cs
+++ Functions/CheckPipelineStatus.cs
@@ -7,10 +7,13 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+
 using Microsoft.Azure.Management.DataFactory;
-using adf = Microsoft.Azure.Management.DataFactory.Models;
 using Microsoft.Azure.Management.Synapse;
+
+using adf = Microsoft.Azure.Management.DataFactory.Models;
 using syn = Microsoft.Azure.Management.Synapse.Models;
+
 using Newtonsoft.Json.Linq;
 using ADFprocfwk.Helpers;
 
@@ -30,7 +33,7 @@ namespace ADFprocfwk
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            string outputString = string.Empty;
+            string requestOutputString = string.Empty;
 
             string tenantId = data?.tenantId;
             string applicationId = data?.applicationId;
@@ -85,74 +88,36 @@ namespace ADFprocfwk
             {
                 using (var client = DataFactoryClient.CreateDataFactoryClient(tenantId, applicationId, authenticationKey, subscriptionId))
                 {
+                    log.LogInformation("Checking ADF pipeline status.");
+
                     //Get pipeline status with provided run id
                     adf.PipelineRun pipelineRun;
                     pipelineRun = client.PipelineRuns.Get(resourceGroup, orchestratorName, runId);
-                    log.LogInformation("Checking ADF pipeline status.");
-
-                    //Create simple status for Data Factory Until comparison checks
-                    string simpleStatus;
-
-                switch (pipelineRun.Status)
-                {
-                    case "InProgress":
-                        simpleStatus = "Running";
-                        break;
-                    case "Canceling":
-                        simpleStatus = "Canceling";
-                        break;
-                    default:
-                        simpleStatus = "Done";
-                        break;
-                }
-
+                    
                     log.LogInformation("ADF pipeline status: " + pipelineRun.Status);
 
                     //Final return detail
-                    outputString = "{ \"PipelineName\": \"" + pipelineName +
-                                            "\", \"RunId\": \"" + pipelineRun.RunId +
-                                            "\", \"SimpleStatus\": \"" + simpleStatus +
-                                            "\", \"Status\": \"" + pipelineRun.Status +
-                                            "\" }";
+                    requestOutputString = CreateOutputString(pipelineName, runId, SetSimpleStatus(pipelineRun.Status), pipelineRun.Status);
                 }
             }
             else if (orchestratorType.ToUpper() == "SYN")
             {
                 using (var client = SynapseClient.CreateSynapseClient(tenantId, applicationId, authenticationKey, subscriptionId))
                 {
+                    log.LogInformation("Checking SYN pipeline status.");
+
                     //Get pipeline status with provided run id
-
                     /*
-                    syn.Workspace pipelineRun; //wrong
-                    //client.
+                    syn.PipelineRun pipelineRun;
                     pipelineRun = client.PipelineRuns.Get(resourceGroup, orchestratorName, runId);
-                    */
-                    adf.PipelineRun pipelineRun;
-                    pipelineRun = client.PipelineRuns.Get(resourceGroup, orchestratorName, runId);
-
-                    log.LogInformation("Checking ADF pipeline status.");
-
-                    //Create simple status for Data Factory Until comparison checks
-                    string simpleStatus;
-
-                    if (pipelineRun.Status == "InProgress")
-                    {
-                        simpleStatus = "Running";
-                    }
-                    else
-                    {
-                        simpleStatus = "Done";
-                    }
 
                     log.LogInformation("ADF pipeline status: " + pipelineRun.Status);
 
                     //Final return detail
-                    outputString = "{ \"PipelineName\": \"" + pipelineName +
-                                            "\", \"RunId\": \"" + pipelineRun.RunId +
-                                            "\", \"SimpleStatus\": \"" + simpleStatus +
-                                            "\", \"Status\": \"" + pipelineRun.Status +
-                                            "\" }";
-                    
+                    requestOutputString = CreateOutputString(pipelineName, pipelineRun.RunId, SetSimpleStatus(pipelineRun.Status), pipelineRun.Status);
+                    */
+
+                    requestOutputString = CreateOutputString(pipelineName, runId, SetSimpleStatus("Unknown"), "Unknown"); //done just just functions builds
                 }
             }
             else
@@ -162,10 +127,45 @@ namespace ADFprocfwk
             }
             #endregion
 
-            JObject outputJson = JObject.Parse(outputString);
+            JObject outputJson = JObject.Parse(requestOutputString);
 
             log.LogInformation("CheckPipelineStatus Function complete.");
             return new OkObjectResult(outputJson);
         }
+
+        #region LocalHelpers
+
+        private static string SetSimpleStatus (string actualStatus)
+        {
+            string simpleStatus;
+
+            switch (actualStatus)
+            {
+                case "InProgress":
+                    simpleStatus = "Running";
+                    break;
+                case "Canceling":
+                    simpleStatus = "Canceling";
+                    break;
+                default:
+                    simpleStatus = "Done";
+                    break;
+            }
+
+            return simpleStatus;
+        }
+
+        private static string CreateOutputString (string pipelineName, string runId, string simpleStatus, string actualStatus)
+        {
+            string outputString = "{ \"PipelineName\": \"" + pipelineName +
+                                "\", \"RunId\": \"" + runId +
+                                "\", \"SimpleStatus\": \"" + simpleStatus +
+                                "\", \"Status\": \"" + actualStatus +
+                                "\" }";
+
+            return outputString;
+        }
+
+        #endregion
     }
 }
